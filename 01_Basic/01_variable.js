8a09cd04ed50a45b47aef73bc5da2a7c77aaa87b
MANOJ KUMAR <97525269+manojGum@users.noreply.github.com>
2023-12-19 05:58:13 +0000

    data type

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountId =123456;
+let accountEmail ="manoj@google.com";
+var accountPassword ="12345";
+accountCity ="Kolkata";
+/*
+Prefer not to use Var
+because of issue in block scope and function scope example it is block scipe {}
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all Js code as newer version
+
+// alert( 3 +3) // we  are using nodejs not browser
+
+console.log("Manoj");
+let name ="Manoj";
+let age = 18;
+let isLoggedIn = false;
+BigInt =2568989885458554558;
